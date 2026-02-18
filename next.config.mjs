2305072f922a6bd72d124aec435ec060320ecbbf
koch titus <kochtitus72@gmail.com>
2026-02-18 14:23:40 +0300

    edited the next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,8 @@ const nextConfig = {
     images: {
         unoptimized: true,
     },
+    // This matches your repository name
+    basePath: "/tetromino-mobile",
 };
 
 export default nextConfig;
